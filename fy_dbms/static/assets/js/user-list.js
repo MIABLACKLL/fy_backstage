d133c548e10a92d4b159cbafa35cf34f97adfb73
scultc <38917890+scultc@users.noreply.github.com>
2019-05-08 22:32:07 +0800

    分页功能 注释掉了加载时的ajax读取

--- fy_dbms/static/assets/js/user-list.js
+++ fy_dbms/static/assets/js/user-list.js
@@ -2,7 +2,7 @@
 window.onload =
     function() {
     userTemplate();
-ajaxUsers();
+//ajaxUsers();
 }
 
 //初始化
@@ -23,70 +23,44 @@ logout() {
   sessionStorage.setItem('isLogin', false);
   window.location.href = '/login';
 }
-
-function ajaxUsers() {
-  console.log('查询用户表');
-  $.ajax({
-    url: 'http://127.0.0.1:8000/user/page/',
-    type: 'post',
-    async: true,
-    dataType: 'json',
-    success: function(result) {
-      console.log(result);
-      usersTemplate(result);
-    },
-    error: function(xhr) {
-      alert(xhr);
-    }
-  })
-}
+//
+//function ajaxUsers() {
+//  console.log('查询用户表');
+//  $.ajax({
+//    url: 'http://127.0.0.1:8000/user/page/',
+//    type: 'post',
+//    async: true,
+//    dataType: 'json',
+//    success: function(result) {
+//      console.log(result);
+//      usersTemplate(result);
+//    },
+//    error: function(xhr) {
+//      alert(xhr);
+//    }
+//  })
+//}
 
 
 /**
  * 使用数据显示user
  * @param  data
  */
-function usersTemplate(data) {
-  var userlist = data;
-  $('#users').html('')
-//  var c = '';
-//  var f = '';
-//  var t = '恢复';
-  $.each(userlist, function(index, item) {
-//    if (item.status == 0) {
-//      c = '';
-//      f = 'rec(' + item.id + ')';
-//      t = '恢复';
-//    } else {
-//      c = 'tpl-table-black-operation-del';
-//      f = 'del(' + item.id + ')';
-//      t = '拉黑';
-//    }
-    $('#users').append($('<tr>').append(
-        $('<td>').append(item.user_id), $('<td>').append(item.name),
-        $('<td>').append(item.phone), $('<td>').append(item.vip=="\u0001"?"是":"否"),
-
-//        $('<td>').append($('<div>').attr('class', 'am-btn-group am-btn-group-xs')
-//                             .append($('<button>')
-//                                         .attr('class','am-btn am-btn-default am-btn-success')
-//                                         .attr('type','button')
+//function usersTemplate(data) {
+//  var userlist = data;
+//  $('#users').html('')
 //
-//                             .append($('<span>')
-//                                         .attr('class','am-icon-file-text-o')
-//                                         )
-//                                          .append($('<button>')
-//                                                   .attr('class','am-btn am-btn-default am-btn-success')
-//                                                   .attr('type','button')
+//  $.each(userlist, function(index, item) {
 //
-//                                          .append($('<span>')
-//                                                   .attr('class','am-icon-trash'))
+//    $('#users').append($('<tr>').append(
+//        $('<td>').append(item.user_id), $('<td>').append(item.name),
+//        $('<td>').append(item.phone), $('<td>').append(item.vip=="\u0001"?"是":"否"),
 //
 //
-//                                         )))
- ));
-})
-
-}
+// ));
+//})
+//
+//}
 function
 search() {
   var nickname =encodeURI($('#input_nickname').val()) ;
